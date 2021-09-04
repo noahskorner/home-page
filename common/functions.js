f828e73f6011737d4c40e3dbfb64311a9fa71391
noah <noahskorner@gmail.com>
2021-09-03 23:27:58 -0500

    Added registration functionality.

---
+++ common/functions.js
@@ -0,0 +1,32 @@
+const pool = require("../db");
+const { emailRegex } = require("../common/constants");
+
+const validateNewUser = async (email, password1, password2) => {
+  const errors = [];
+
+  try {
+    const result = await pool.query(
+      "SELECT email FROM users WHERE email = $1 LIMIT 1",
+      [email]
+    );
+
+    if (result.rows[0]) errors.push(`User with email ${email} already exists.`);
+    if (!emailRegex.test(email)) errors.push("Email must be valid.");
+    if (password1 !== password2) errors.push("Passwords do not match.");
+    if (password1.length < 8)
+      errors.push("Password must be at least 8 characters.");
+    if (!/\d/.test(password1))
+      errors.push("Password must contain at least 1 number.");
+    if (!/[A-Z]/.test(password1))
+      errors.push("Password must contain at least 1 uppercase letter.");
+
+    return errors;
+  } catch (error) {
+    console.log(error.message);
+    errors.push("An unexpected error occurred. Please try again.");
+  }
+};
+
+module.exports = {
+  validateNewUser,
+};

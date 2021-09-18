f3cbd94b59a4e4fe4cdb6c534057f36e479ee94f
noah <noahskorner@gmail.com>
2021-09-17 21:21:34 -0500

    Added logout API.

--- server/common/constants.js
+++ server/common/constants.js
@@ -1,8 +1,8 @@
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const accessTokenExpiration = "60s";
-const refreshTokenExpiration = 5;
+const accessTokenExpiration = 1800; //seconds = 15 minutes
+const refreshTokenExpiration = 15; //days
 
 module.exports = {
   emailRegex,

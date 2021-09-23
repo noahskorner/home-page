2903f4a173f93d4171161d2ee913215e1f064ed4
noah <noahskorner@gmail.com>
2021-09-22 21:32:33 -0500

    Added created swimlanes entity, modified todos entity, created swimlanes POST, todos POST, and todos GET.

--- server/database.sql
+++ server/database.sql
@@ -2,19 +2,31 @@ CREATE DATABASE homepage;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    date_created TIMESTAMP DEFAULT NOW(),
     email VARCHAR(255),
     password VARCHAR(255)
 );
 
+CREATE TABLE swimlanes (
+    id SERIAL PRIMARY KEY,
+    date_created TIMESTAMP DEFAULT NOW(),
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE todos (
     id SERIAL PRIMARY KEY,
+    date_created TIMESTAMP DEFAULT NOW(),
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    swimlane_id INTEGER REFERENCES swimlanes(id) ON DELETE CASCADE,
     title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) DEFAULT ''
+    description VARCHAR(255) DEFAULT '',
+    status INTEGER DEFAULT 0
 );
 
 CREATE TABLE refresh_tokens (
     token VARCHAR(255) PRIMARY KEY,
+    date_created TIMESTAMP DEFAULT NOW(),
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     valid_until TIMESTAMP NOT NULL
 );

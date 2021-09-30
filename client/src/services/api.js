66da2ff9004fd96b4138dc3a8706ef67a65dcf70
noah <noahskorner@gmail.com>
2021-09-30 15:47:38 -0500

    Added PORT.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
-//const baseURL = "https://home-page-server.herokuapp.com/";
+//const baseURL = "http://localhost:3000";
+const baseURL = "https://home-page-server.herokuapp.com/";
 
 const apiClient = axios.create({
   baseURL: baseURL,
